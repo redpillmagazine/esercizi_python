48ce43934e633c0758b7474d784004d42099ae01
redpillmagazine <74100105+redpillmagazine@users.noreply.github.com>
2022-12-28 16:18:27 +0100

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+num = int(input("Inserisci il numero da convertire: "))
+print("Ecco i valori convertiti di ", num, ":")
+print(bin(num), "in binario.")
+print(oct(num), "in ottale.")
+print(hex(num), "in esadecimale.")
